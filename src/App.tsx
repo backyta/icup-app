fcea5d309ca67ea635b6b975d4f8d311409b467a
Kevin_BcAng <kevi.baca18@gmail.com>
2024-02-02 16:27:22 -0500

    chore: :tada: initial configs, istalled init depedencies, created structure basic

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <Button>Hola mundo</Button>
+    </>
+  )
+}
+
+export default App
