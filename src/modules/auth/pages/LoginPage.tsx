3130c2380739ec432c09f33a78cc49d4b1896dbd
Kevin_BcAng <kevi.baca18@gmail.com>
2025-01-05 14:01:05 -0500

    fix: :bug: bugs fixed in login pagin and fallbacks added

--- src/modules/auth/pages/LoginPage.tsx
+++ src/modules/auth/pages/LoginPage.tsx
@@ -124,12 +124,10 @@ export const LoginPage = (): JSX.Element => {
           alt='Logo Iglesia'
           className='w-[30rem] md:w-[33rem] lg:w-[37rem] xl:w-[45rem] pb-8 pt-[6rem] md:pt-[4rem] mx-auto'
         />
-        <h1 className='text-4xl md:text-5xl lg:text-[3.60rem] xl:text-7xl text-center font-semibold text-blue-800 font-dancing-script mb-0 md:mb-4 lg:mb-4 xl:mb-2 xl:leading-[4.5rem]'>
+        <h1 className='text-4xl md:text-5xl lg:text-[3.60rem] xl:text-7xl text-center font-semibold dark:text-blue-600 text-blue-800 font-dancing-script mb-0 md:mb-4 lg:mb-4 xl:mb-2 xl:leading-[4.5rem]'>
           ICUP APP
         </h1>
-        <h2 className='text-center text-[1.4rem] md:text-[1.7rem] lg:text-3xl xl:text-4xl p-2 md:pb-4 italic'>
-          Bienvenido
-        </h2>
+        <h2 className='text-center text-[1.4rem] md:text-[1.7rem] lg:text-3xl xl:text-4xl p-2 md:pb-4 italic'></h2>
       </div>
 
       <div className='mx-auto w-[18rem] md:w-[24rem] md:mx-auto sm:max-w-md px-5 pb-7 pt-5 md:px-10 md:pt-8 md:pb-10 shadow-md overflow-hidden sm:rounded-lg bg-white dark:bg-slate-900'>
@@ -214,7 +212,12 @@ export const LoginPage = (): JSX.Element => {
               <Button
                 disabled={isInputDisabled}
                 type='submit'
-                className='w-full text-[14px]'
+                className={cn(
+                  'w-full text-[14px]',
+                  isInputDisabled &&
+                    statusCode !== 429 &&
+                    'dark:bg-teal-500 bg-teal-600 text-[15px] text-white'
+                )}
                 onClick={() => {
                   setTimeout(() => {
                     if (Object.keys(form.formState.errors).length === 0) {
@@ -223,7 +226,7 @@ export const LoginPage = (): JSX.Element => {
                   }, 100);
                 }}
               >
-                Iniciar Sesión
+                {isInputDisabled && statusCode !== 429 ? 'Conectando...' : 'Iniciar Sesión'}
               </Button>
             </div>
           </form>
