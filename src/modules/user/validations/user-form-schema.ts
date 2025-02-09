e0880a6ea972e83ec947e5839538e93c1527bd31
Kevin_BcAng <kevi.baca18@gmail.com>
2025-02-08 20:02:21 -0500

    fix: :bug: updated regex validation

--- src/modules/user/validations/user-form-schema.ts
+++ src/modules/user/validations/user-form-schema.ts
@@ -35,9 +35,7 @@ export const userFormSchema = z
     password: z
       .string()
       .regex(
-        new RegExp(
-          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$'
-        ),
+        new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$'),
         'La contraseña no cumple con los requisitos mínimos'
       )
       .optional(),
@@ -45,9 +43,7 @@ export const userFormSchema = z
     passwordConfirm: z
       .string()
       .regex(
-        new RegExp(
-          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$'
-        ),
+        new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$'),
         'La contraseña no cumple con los requisitos mínimos'
       )
       .optional(),
